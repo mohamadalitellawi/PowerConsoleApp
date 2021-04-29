7a17922409f4a53ce2d369acc24c05da1a72c029
Ali <mohamadalitellawi@gmail.com>
2021-04-29 17:21:05 +0300

    test and run correctly

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using System;
@@ -6,38 +7,40 @@ using System.IO;
 
 namespace ConsoleUI
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var builder = new ConfigurationBuilder();
-            BuildConfig(builder);
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var builder = new ConfigurationBuilder();
+			BuildConfig(builder);
 
-            Log.Logger = new LoggerConfiguration()
-                .ReadFrom.Configuration(builder.Build())
-                .Enrich.FromLogContext()
-                .WriteTo.Console()
-                .CreateLogger();
+			Log.Logger = new LoggerConfiguration()
+				.ReadFrom.Configuration(builder.Build())
+				.Enrich.FromLogContext()
+				.WriteTo.Console()
+				.CreateLogger();
 
-            Log.Logger.Information("Application Starting");
+			Log.Logger.Information("Application Starting");
 
-            var host = Host.CreateDefaultBuilder()
-                .ConfigureServices((context, services) =>
-                {
+			var host = Host.CreateDefaultBuilder()
+				.ConfigureServices((context, services) =>
+				{
+					services.AddTransient<IGreetingService, GreetingService>();
+				})
+				.UseSerilog()
+				.Build();
 
-                })
-                .UseSerilog()
-                .Build();
+			var service = ActivatorUtilities.CreateInstance<GreetingService>(host.Services);
+			service.Run();
+		}
 
+		private static void BuildConfig(IConfigurationBuilder builder)
+		{
+			builder.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+				.AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+				.AddEnvironmentVariables();
+		}
+	}
 
-        }
-
-        private static void BuildConfig(IConfigurationBuilder builder)
-        {
-            builder.SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
-                .AddEnvironmentVariables();
-        }
-    }
 }
